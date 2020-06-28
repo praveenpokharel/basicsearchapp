e9fd7c5e307c8e26d135f15096c1261228da11bf
praveenpokharel <pokhrel.praveen@gmail.com>
2020-06-28 15:26:28 +0200

    Updates

---
+++ precache-manifest.15f67090133d8f68cdf69aa909c4520e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2e9bea630be55a1f7a69b287385b3a",
+    "url": "/basicsearchapp/index.html"
+  },
+  {
+    "revision": "a6e9a06384d41a2505de",
+    "url": "/basicsearchapp/static/css/main.767f9ab8.chunk.css"
+  },
+  {
+    "revision": "d1c84f66b7a4945a6c0b",
+    "url": "/basicsearchapp/static/js/2.26be0ce2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basicsearchapp/static/js/2.26be0ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e9a06384d41a2505de",
+    "url": "/basicsearchapp/static/js/main.2da52fc5.chunk.js"
+  },
+  {
+    "revision": "fbedec315bac446ae924",
+    "url": "/basicsearchapp/static/js/runtime-main.c5093873.js"
+  }
+]);
